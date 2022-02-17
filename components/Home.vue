c3c8a2f5c60df6dffa0619831dab2e961b9be2ff
HAjulien <96483338+HAjulien@users.noreply.github.com>
2022-02-17 14:43:00 +0100

    commit

---
+++ components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="home bg-red-300">
+    <h1 class="text-red-700	"> test</h1>
+  <p class="text-red-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis voluptatibus tempore corrupti odio blanditiis dolores optio sunt! Beatae tempora impedit facilis, explicabo unde ex quas fugit autem necessitatibus. Totam, in!</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+}
+</script>
+
+<style lang="scss" scoped>
+    .home{
+        position: relative;
+        height: 300px;
+    }
+  p{
+    color: $green;
+  @include absolutePosition(45%,25%,35%,25%);
+}
+  
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <nav class="flex justify-center">
+    <NuxtLink to="/">Page accueil</NuxtLink> |
+    <NuxtLink to="/about">test</NuxtLink>
+  </nav>
+  <Home/>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+
+<style lang="scss" >
+
+  
+</style>
