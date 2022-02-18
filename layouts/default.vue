884f58106c3c09216b51e7dba27c2e86444e4b1b
HAjulien <96483338+HAjulien@users.noreply.github.com>
2022-02-18 09:49:50 +0100

    go

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="App">
+        <HeaderMobile />
+        <Nuxt />
+        <Navigation />
+        <FooterMobile />
+    </div>
+</template>
+
+<script>
+
+import headerMobile from '~/components/headerMobile.vue'
+import navigation from '~/components/navigation.vue'
+import footerMobile from '~/components/footerMobile.vue'
+
+
+export default {
+  name: 'AppPage',
+  components: { navigation, headerMobile, footerMobile },
+}
+
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,15 @@
 <template>
 <div>
-  <HeaderMobile />
-  <Navigation />
-  <FooterMobile />
+
 </div>
 
 </template>
 
 <script>
-import headerMobile from '~/components/headerMobile.vue'
-import navigation from '~/components/navigation.vue'
-import footerMobile from '~/components/footerMobile.vue'
+
 export default {
   name: 'IndexPage',
-  components: { navigation, headerMobile, footerMobile },
+  components: { },
 }
 </script>
 
