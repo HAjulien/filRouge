d8ce27a1cc0a7a74ae59f343516a5070f52a0ae4
HAjulien <96483338+HAjulien@users.noreply.github.com>
2022-02-23 09:44:27 +0100

    Update footerMobile.vue

--- components/footerMobile.vue
+++ components/footerMobile.vue
@@ -10,17 +10,49 @@
             <li><a href="https://twitter.com" target="_blank"><font-awesome-icon :icon="['fab', 'twitter']" class="fa-2x" /></a> </li>
         </ul>
         <p class="text-center">CANTINE MIAM tous droits réservés, 2022</p>
+        <div>
+            <transition name="bounce">
+            <p v-if="active" class="test"><HeaderMobile /></p>
+            </transition>
+            <button @click="active = !active" >Toggle active</button>
+        </div>
+        
 
     </footer>
 </template>
 
 <script>
+
+import headerMobile from '~/components/headerMobile.vue'
+
 export default {
   name: 'FooterMobile',
+  components: { headerMobile },
+  data(){
+      return{
+          active: false,
+      }
+  },
 }
 </script>
 
 <style lang="scss" scoped>
    
-  
+    .bounce-enter-active{
+        animation: bounce-in .5s;
+    }
+    .bounce-leave-active{
+        animation: bounce-in .5s reverse;
+    }
+    @keyframes bounce-in{
+        0%{
+            transform: scale(0);
+        }
+        50%{
+            transform: scale(1.5);
+        }
+        100%{
+            transform: scale(1);
+        }
+    }
 </style>
